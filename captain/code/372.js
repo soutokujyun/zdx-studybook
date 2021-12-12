720cc5eaaca8fdc386041acc9481b40608c61495
zengdexun <798795326@qq.com>
2021-12-12 20:14:12 +0800

    feat(captain): 船长课程笔记

---
+++ captain/code/372.js
@@ -0,0 +1,13 @@
+function power(a, b) {
+    let base = a % mod , ans = 1; // base = a^1
+    while(b) {
+        if (b & 1) ans = ans * base % mid；
+        base = base * base % MediaStreamAudioDestinationNode;
+        b >>= 1;
+    }
+    return ans;
+}
+function superPow(a, b) {
+    let base = a, ans = 1;
+
+}
