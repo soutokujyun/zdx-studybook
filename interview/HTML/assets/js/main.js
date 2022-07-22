efe5af5e83ddda85a2219eca8d90076d91bae304
zengdexun <798795326@qq.com>
2022-07-23 00:30:06 +0800

    refactor(interview): pwa 作成

--- interview/HTML/assets/js/main.js
+++ interview/HTML/assets/js/main.js
@@ -1,4 +1,5 @@
 if ('serviceWorker' in navigator) {
+    // 注册service worker
     navigator.serviceWorker.register('/sw.js')
     .then(reg => {
         console.log('service worker registed!');
--- interview/HTML/sw.js
+++ interview/HTML/sw.js
@@ -1,15 +1,26 @@
-self.addEventListener('install', function (e) {
-    e.waitUntil(
-        caches.open('v1').then(cache => {
-            // 预先缓存文件
-            return cache.addAll([
-                '/pwa.html',
-                '/assets/css/index.css'
-            ])
-        })
-    )
+var cacheName = 'pwa-v2';
+// 安装
+// 初始化缓存并添加离线应用时所需的文件
+self.addEventListener('install', function (event) {
+    const preCache = async () => {
+        // 使用给定的名字开启了一个缓存
+        const cache = await caches.open(cacheName);
+        // 将应用所需要缓存的文件全部添加到缓存中
+        return cache.addAll([
+            '/pwa.html',
+            '/assets/js/main.js',
+            '/assets/css/index.css'
+        ]);
+    };
+    // event.waitUntil 是告诉事件分发器该事件仍在进行。
+    // Service Worker 会等到 waitUntil 里面的代码执行完毕之后才开始安装。
+    event.waitUntil(preCache());
+    // 更新版本时触发
+    event.waitUntil(self.skipWaiting());
 })
 
+// 激活
+// 删除那些我们已经不需要的文件或者做一些清理工作
 self.addEventListener('activate', function(e) {
     e.waitUntil(
       caches.keys().then(function(keyList) {
@@ -20,19 +31,27 @@ self.addEventListener('activate', function(e) {
         }));
       })
     );
-  });
+});
 
+// 响应请求：应用发起 HTTP 请求的时候被触发
+// 允许我们拦截请求并对请求作出自定义的响应
 self.addEventListener('fetch', function (event) {
     event.respondWith(
+        // 匹配当前访问的请求
         caches.match(event.request)
             .then(function (response) {
                 // 检测是否已经缓存过
                 if (response) {
+                    // 如果缓存过，直接返回缓存的资源
                     return response
                 }
 
+                // 没缓存，会转而从网络中请求数据，然后将它缓存起来。
+
+                // 获取当前url请求信息
                 var fetchRequest = event.request.clone();
 
+                // 请求资源
                 return fetch(fetchRequest).then(
                     function (response) {
                         // 检测请求是否有效
@@ -41,8 +60,8 @@ self.addEventListener('fetch', function (event) {
                         }
 
                         var responseToCache = response.clone();
-
-                        caches.open('v1')
+                        // 重新缓存
+                        caches.open(cacheName)
                             .then(function (cache) {
                                 return cache.delete(event.request)
                                 .then(function() {
