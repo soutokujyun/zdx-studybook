a10164fc9d52bc8cd486d086266ff3c9c54955f8
mid1683 <798795326@qq.com>
2022-08-04 14:58:56 +0800

    feat(interview): event bus

--- interview/JS/eventBus.js
+++ interview/JS/eventBus.js
@@ -1,2 +1,21 @@
 
 // 使用
+class eventBus {
+    constructor() {
+        this.events = {}
+    }
+
+    emit(eventName, ...args) {
+        if (this.events[eventName]) {
+            this.events[eventName].forEach(fn => fn.apply(this, args))
+        }
+    }
+
+    on(eventName, fn) {
+        if (this.events[eventName]) {
+            this.events[eventName].push(fn)
+        } else {
+            this.events[eventName] = [fn]
+        }
+    }
+}
