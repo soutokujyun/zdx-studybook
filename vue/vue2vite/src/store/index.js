0f3b731cf9d33b7b3ae3d47662aed9453312c236
mid1683 <798795326@qq.com>
2022-09-13 13:08:00 +0800

    vue2vite

--- vue/vue2vite/src/store/index.js
+++ vue/vue2vite/src/store/index.js
@@ -3,13 +3,32 @@ import Vuex from 'vuex'
 
 Vue.use(Vuex)
 
+// https://webpack.js.org/guides/dependency-management/#requirecontext
+let modulesFiles = null
+let modules = {}
+if (import.meta.env.VITE_KEY == 'vite') {
+  modulesFiles = import.meta.globEager('./modules/**/*.js')
+  // eslint-disable-next-line no-unused-vars
+  Object.keys(modulesFiles).forEach(modulePath => {
+    const moduleName = modulePath.replace(/^\.\/.*\/(.*)\.\w+$/, '$1')
+    const value = modulesFiles[modulePath]
+    modules[moduleName] = value.default
+  })
+} else {
+  modulesFiles = require.context('./modules', true, /\.js$/)
+
+  // you do not need `import app from './modules/app'`
+  // it will auto require all vuex module from modules file
+  modules = modulesFiles.keys().reduce((modules, modulePath) => {
+  // set './app.js' => 'app'
+    const moduleName = modulePath.replace(/^\.\/(.*)\.\w+$/, '$1')
+    const value = modulesFiles(modulePath)
+    modules[moduleName] = value.default
+    return modules
+  }, {})
+}
+
 export default new Vuex.Store({
-  state: {
-  },
-  mutations: {
-  },
-  actions: {
-  },
-  modules: {
-  }
+  modules,
+  // getters
 })
--- vue/vue2vite/vite.config.js
+++ vue/vue2vite/vite.config.js
@@ -1,42 +1,75 @@
 import { defineConfig, loadEnv } from 'vite'
 import legacy from '@vitejs/plugin-legacy'
-import { createVuePlugin } from 'vite-plugin-vue2'
-import viteCompression from 'vite-plugin-compression';
+import vue from '@vitejs/plugin-vue2'
+import viteCompression from 'vite-plugin-compression'
+import { viteCommonjs } from '@originjs/vite-plugin-commonjs'
 import path from 'path';
 
-const HOST = "0.0.0.0"
-const REPLACEMENT = `${path.resolve(__dirname, './src')}/`
+const HOST = '0.0.0.0'
+const resolve = (dir) => path.join(__dirname, dir)
 
 export default ({ mode }/** if you want to use mode : { mode }*/) => {
   const env = loadEnv(mode, process.cwd())
-  console.log(env)
   return defineConfig({
-    base: "./",
+    base: './',
     server: {
       host: HOST,
       port: env.VITE_PORT,
+      proxy: {
+        '/restApi': {
+          target: env.VITE_PROXY_TARGET,
+          changeOrigin: true,
+          rewrite: (path) => path.replace(/^\/restApi/, '')
+        }
+      }
     },
+    esbuild: {
+      jsxFactory: 'h',
+      jsxFragment: 'Fragment',
+      jsxInject: 'import { h } from "vue";',
+    },
+    define: {'process.env': {}},
     resolve: {
       alias: [
         {
-          find: '@/',
-          replacement: REPLACEMENT,
+          find: '@',
+          replacement: resolve('src'),
         },
         {
-          find: 'src/',
-          replacement: REPLACEMENT,
+          find: '@assets',
+          replacement: resolve('src/assets'),
+        },
+        {
+          find: '@components',
+          replacement: resolve('src/components'),
+        },
+        {
+          find: '@views',
+          replacement: resolve('src/views'),
+        },
+        {
+          find: '@store',
+          replacement: resolve('src/store'),
+        },
+        {
+          find: '@styles',
+          replacement: resolve('src/styles'),
+        },
+        {
+          find: '@mixins',
+          replacement: resolve('src/mixins'),
         },
       ],
+      extensions: ['.mjs', '.js', '.ts', '.jsx', '.tsx', '.json', '.vue']
     },
     plugins: [
-      createVuePlugin(/* options */),
+      vue(),
       legacy({
         targets: ['ie >= 11'],
         additionalLegacyPolyfills: ['regenerator-runtime/runtime'],
       }),
-      viteCompression()
+      viteCompression(),
+      viteCommonjs({ skipPreBuild: true }),
     ],
   })
 }
-
-
